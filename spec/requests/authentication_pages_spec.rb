01cc66964c70dae79781aefe39cc12a525065698
nicolafeldman <nicola.feldman@yale.edu>
2014-07-02 14:16:21 -0400

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,6 +15,19 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
@@ -99,7 +112,7 @@ describe "Authentication" do
 
   	describe "with valid information" do
   		let(:user) { FactoryGirl.create(:user) }
-  		before { sign_in(user) }
+  		before { sign_in user }
 
   		it { should have_title(user.name) }
       it { should have_link('Users',       href: users_path) }
